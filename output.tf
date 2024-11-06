1df2c05040475b81372fea66c4e4d837bb9d29d1
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-06 14:31:19 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "private_ip" {
-  value       = "${aws_instance.tomcat.*.private_ip}"
+  value       = "${aws_instance.jenkins.*.private_ip}"
   description = "PrivateIP address details"
 } 
 
 output "public_ip" {
-  value       = "${aws_instance.tomcat.*.public_ip}"
+  value       = "${aws_instance.jenkins.*.public_ip}"
   description = "PrivateIP address details"
 }  
