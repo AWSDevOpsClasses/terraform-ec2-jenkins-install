a71ad60e00552373dafc72ef6223a425397a9350
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-07 18:08:57 +0530

    Update jenkins-sg.tf

--- jenkins-sg.tf
+++ jenkins-sg.tf
@@ -1,5 +1,7 @@
 # Security group resource for Ubuntu server
 resource "aws_security_group" "jenkinssg" {
+  name                = "jenkins-SG"
+  description         = "Security Group for Jenkins Server"
   egress = [
     {
       cidr_blocks      = [ "0.0.0.0/0", ]
@@ -18,25 +20,29 @@ resource "aws_security_group" "jenkinssg" {
      cidr_blocks      = [ "0.0.0.0/0", ]
      description      = "ssh"
      from_port        = 22
+#     from_port        = local.ssh_port
      ipv6_cidr_blocks = []
      prefix_list_ids  = []
      protocol         = "tcp"
      security_groups  = []
      self             = false
-     to_port          = 22
+  #   to_port          = 22
+     to_port          = local.ssh_port
    },
   #]
   #ingress                = 
    {
      cidr_blocks      = [ "0.0.0.0/0", ]
      description      = "jenkins"
-     from_port        = 8080
+#     from_port        = 8080
+     from_port        = local.jenkins_port
      ipv6_cidr_blocks = []
      prefix_list_ids  = []
      protocol         = "tcp"
      security_groups  = []
      self             = false
-     to_port          = 8080
+#     to_port          = 8080
+     to_port          = local.jenkins_port
    },
   ]
 /*  tags = {
