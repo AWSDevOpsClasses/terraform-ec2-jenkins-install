2ced03eb2a6f6d2f43486f5fcae9b9e36e4521ee
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-07 10:32:31 +0530

    Create jenkins-sg.tf

---
+++ jenkins-sg.tf
@@ -0,0 +1,44 @@
+# Security group resource for Ubuntu server
+resource "aws_security_group" "jenkinssg" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = "ssh"
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+   },
+  #]
+  #ingress                = 
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = "jenkins"
+     from_port        = 8080
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 8080
+   },
+  ]
+/*  tags = {
+  Name = “name_security_group” 
+  } */
