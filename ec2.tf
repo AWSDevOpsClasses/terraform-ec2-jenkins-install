ad720b7a40d71093c59e1067870915b7effb2e6e
srinivasateja <srinivas.21may@gmail.com>
2024-11-11 14:41:42 +0530

    updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,21 +12,24 @@ data "aws_ami" "ubuntu" {
 
 # Ec2 resource for Ubuntu Server
 resource "aws_instance" "jenkins" {
-#  ami           = "ami-0866a3c8686eaeeba"
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.medium"
-  key_name      = "nvirginia-devops"
-  count = 1
+#  ami              = "ami-0866a3c8686eaeeba"
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = "t3.medium"
+  availability_zone = "us-east-1a"
+  key_name          = "nvirginia-devops"
+  count             = 1
 /*  vpc_security_group_ids = [
     "sg-012f058de412a1366"
   ] */
-  vpc_security_group_ids = ["${aws_security_group.jenkinssg.id}"]
-   
+#  vpc_security_group_ids = ["${aws_security_group.jenkinssg.id}"]
+  security_groups  = [aws_security_group.jenkinssg.id]
+#  subnet_id = "subnet-0f7f1a84a20b5d114" 
+  subnet_id        = aws_subnet.app-pubsubnet.id 
    #USERDATA in AWS EC2 using Terraform
 #  user_data = file("userdata-jenkins.sh")
 #  user_data = "${file("userdata-jenkins.sh")}"
 #  user_data = file("${path.module}/userdata-jenkins.sh")
-  subnet_id = "subnet-0f7f1a84a20b5d114"
+
   user_data = templatefile("user_data/userdata-jenkins.sh",
     {
       ServerName = var.ServerName
